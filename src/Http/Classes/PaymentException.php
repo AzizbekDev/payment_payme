4e7efbfcbd13ac594efd74f1e8c5ffd9c02f7683
AzizbekDev <36416819+AzizbekDev@users.noreply.github.com>
2022-01-11 01:29:31 +0500

    payme exception file created

---
+++ src/Http/Classes/PaymentException.php
@@ -0,0 +1,24 @@
+<?php
+namespace Payment\Payme\Http\Classes;
+
+
+class PaymentException extends \Exception{
+
+    public $response;
+    
+    public function __construct($response)
+    {
+        $this->response = $response;
+    }
+    
+    public function setReponse($reponse)
+    {
+        $this->response = $reponse;
+    }
+
+    public function response(){
+        return $this->response->send();
+    }
+    
+
+}
